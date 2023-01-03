745fc7efbff89a417f31a8c7571a1cda601bc9c5
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2023-01-04 00:19:09 +0400

    Add files via upload

---
+++ JS-2 Dersleri/App.js
@@ -0,0 +1,120 @@
+
+
+/* Scope */
+
+/* var let const */
+
+/* 
+Global
+Local
+Block
+
+*/
+
+/* {
+    var a = 5;
+    
+}
+
+console.log(a); */
+ 
+/* const a = 5;
+{
+    console.log(a);
+}
+
+
+ function myFunc(){
+    
+        console.log(a);
+ }
+
+    myFunc(); */
+
+
+    /* Massivler  Arrays */
+
+/*  let name = "Ramin";
+ let surname = "Israfilli"
+ let age = 32;
+ let work = true;
+
+
+ let myArr = ["Ramin","Israfilli",32,true];
+ myArr[0] = "Neriman";
+ console.log(myArr[0]);
+
+ let baza = [1,2,3,4,5];
+ let Telebeler = [ "Ramin","Elsen","Aysu"]; */
+
+
+/*  const avtomobil = {
+        marka: "KIA",
+        rengi: "Ag",
+        sureti:320,
+        il:2014,
+        myfunc:function(){
+            let x = 33;
+           return x;
+        }
+ }
+       avtomobil.myfunc();
+    avtomobil.sureti;
+
+    avtomobil.sureti = 500;
+
+    console.log(avtomobil.sureti);
+    console.log(avtomobil); */
+
+
+/*     const myObj = [
+        {
+            name: "Ramin",
+            surname: "Israfilli"
+        },
+        {
+            name: "Elsad",
+            surname: "Israfilli"
+        },
+        {
+            name: "Yusif",
+            surname: "Israfilli"
+        },
+        {
+            name: "Ramin",
+            surname: "Israfilli"
+        },
+        {
+            name: "Ramin",
+            surname: "Israfilli"
+        },
+    ] */
+
+    
+
+    /* Dovurler */
+
+  /* for  while do while */
+/*   let i = 1;
+  for(;i <= 10;console.log(i)){
+    i++;
+  } */
+
+
+/*   let x = 11;
+while(x <= 10){
+    console.log(x);
+    x++;
+}
+
+
+console.log("---------------------");
+let i = 11;
+do{
+    console.log(i);
+    i++;
+}while(i<10); */
+
+  
+
+   
